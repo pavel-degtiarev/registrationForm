5277b2878d400ff87aa21071cf8885498e986c29
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-09-01 21:08:48 +0700

    WIP добавлены функции валидации вместо заглушек

--- src/js/checks.js
+++ src/js/checks.js
@@ -1,24 +1,103 @@
 // eslint-disable-next-line no-unused-vars
 import { FIELD_ERROR, FIELD_OK } from "./global";
 
-// валидатор на вход получает значение поля, объект ValidityState и объект formState
+// {
+//   badInput: false;
+//   customError: false;
+//   patternMismatch: false;
+//   rangeOverflow: false;
+//   rangeUnderflow: false;
+//   stepMismatch: false;
+//   tooLong: false;
+//   tooShort: false;
+//   typeMismatch: false;
+//   valid: false;
+//   valueMissing: true;
+// }
 
-export function name(value, validity, state) {
-  console.log("firstName", value, validity, state);
-  return { result: FIELD_ERROR, message: "Очень страшная ошибка в firstName" };
+// валидатор на вход получает объект ValidityState и объект formState
+
+// =============================================
+
+export function name(validity, state) {
+  console.log("firstName", validity, state);
+
+  switch (true) {
+    case validity.patternMismatch:
+      return {
+        result: FIELD_ERROR,
+        message: "Разрешены символы кириллицы, латиницы и дефис.",
+      };
+    case validity.tooLong:
+      return {
+        result: FIELD_ERROR,
+        message: "Слишком много текста. Допускается не более 255 символов.",
+      };
+    case validity.valueMissing:
+      return {
+        result: FIELD_ERROR,
+        message: "Поле обязательно должно быть заполнено.",
+      };
+  }
+
+  return { result: FIELD_OK, message: "" };
 }
 
-export function email(value, validity, state) {
-  console.log("email", value, validity, state);
-  return { result: FIELD_ERROR, message: "Очень страшная ошибка в email" };
+// =============================================
+
+export function email(validity, state) {
+  console.log("email", validity, state);
+
+  switch (true) {
+    case validity.typeMismatch:
+      return {
+        result: FIELD_ERROR,
+        message: "Неправильный формат email.",
+      };
+    case validity.tooLong:
+      return {
+        result: FIELD_ERROR,
+        message: "Слишком длинный адрес. Допускается не более 255 символов.",
+      };
+    case validity.valueMissing:
+      return {
+        result: FIELD_ERROR,
+        message: "Поле обязательно должно быть заполнено.",
+      };
+  }
+
+  return { result: FIELD_OK, message: "" };
 }
 
-export function password(value, validity, state) {
-  console.log("pass", value, validity, state);
-  return { result: FIELD_ERROR, message: "Очень страшная ошибка в pass" };
+// =============================================
+
+export function password(validity, state) {
+  console.log("pass", validity, state);
+
+  switch (true) {
+    case validity.tooLong:
+      return {
+        result: FIELD_ERROR,
+        message: "Слишком длинный пароль. Допускается не более 255 символов.",
+      };
+    case validity.tooShort:
+      return {
+        result: FIELD_ERROR,
+        message: "Слишком короткий пароль. Должно быть не менее 8 символов.",
+      };
+    case state.pass && state.passRepeat && state.pass !== state.passRepeat:
+      return {
+        result: FIELD_ERROR,
+        message: "Введенные пароли не совпадают.",
+      };
+  }
+
+  return { result: FIELD_OK, message: "" };
 }
 
-export function birthDate(value, validity, state) {
-  console.log("birthDate", value, validity, state);
+// =============================================
+
+export function birthDate(validity, state) {
+  console.log("birthDate", validity, state);
   return { result: FIELD_ERROR, message: "Очень страшная ошибка в birthDate" };
 }
