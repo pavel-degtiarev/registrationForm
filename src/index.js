1b5266642cac973a5b3b63965aa82e8717f9701f
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-09-01 18:08:49 +0700

    создается и заполняется объект FormState

--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@
 
 import "./index.scss";
 
-console.log("loaded");
+import { FormState } from "./js/formState";
+import { Validator } from "./js/validator";
+import * as check from "./js/checks";
+
+const formID = "regForm";
+
+// ключи – id input'ов из HTML, значения – функции-валидаторы этого поля
+// валидатор на вход получает значение поля, объект ValidityState и объект formState
+export const fieldChecks = {
+  firstName: check.name,
+  lastName: check.name,
+  email: check.email,
+  pass: check.password,
+  passRepeat: check.password,
+  birthDate: check.birthDate,
+};
+
+const formState = new FormState(formID, new Validator());
+
+Object.keys(fieldChecks).forEach((id) => {
+  formState.addField(id, fieldChecks[id]);
+});
