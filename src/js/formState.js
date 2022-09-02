31ae85d99bf6d4599cdee843d443caff346d6ff6
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-09-02 13:34:11 +0700

    конструктор FormState получает уже инициализированный валидатор

--- src/js/formState.js
+++ src/js/formState.js
@@ -3,23 +3,24 @@ import { Field } from "./field";
 export class FormState {
   state = {};
   fields = new Map();
+  validator = null;
 
   constructor(formID, validator) {
     this.form = document.getElementById(formID);
     if (!this.form) throw new Error("Форма не найдена!");
 
     this.validator = validator;
+
     this.fieldBlurHandler = this.fieldBlurHandler.bind(this);
   }
 
-  addField(id, check) {
+  addField(id) {
     const DOMfield = document.getElementById(id);
     if (!DOMfield) throw new Error(`Поле с ID:${id} не найдено!`);
 
     const fieldObj = new Field(DOMfield, this.fieldBlurHandler);
     this.fields.set(DOMfield, fieldObj);
 
-    this.validator.addCheck(id, check);
     this.state[id] = "";
   }
 
